35bd58155f6688d733e3377290bb5f6791329963
ulaskarakas <ulaskarakas95@gmail.com>
2025-01-17 15:24:18 +0300

    Add project files.

---
+++ PatikaTryCatchApp/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        bool isValidValue = false;
+
+        while (!isValidValue)
+        {
+            try
+            {
+                Console.Write("Please enter a number: ");
+                int number = int.Parse(Console.ReadLine());
+
+                isValidValue = true;
+
+                Console.WriteLine($"{number} to power of 2: {Math.Pow(number, 2)}");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid value! Please enter a number.");
+            }
+        }
+
+    }
+}
